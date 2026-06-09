50e7dff29bd3b6f44bc64455bdb19a5f5d6a31a0
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-09 22:42:42 +0530

    set user Model

---
+++ models/userModels.js
@@ -0,0 +1,44 @@
+const userModel = require("../db/schemas/userSchema");
+
+const createUser = async(user) => {
+
+    try{
+
+        const newUser = await userModel.create({
+        name : user.name,
+        email : user.email,
+        password : user.password
+
+    })
+
+    return newUser;
+
+    } catch(error){
+
+        console.log(`Error occured at createUser model, error : ${error}`);
+        return false;
+
+    }
+}
+
+const retriveUser = async(email) => {
+
+    try{
+
+        const user = await userModel.findOne({email:email});
+
+        return user;
+
+
+    } catch(error){
+
+        console.log(`Error occured at retriveUser model, error : ${error}`);
+        return false;
+    }
+
+}
+
+module.exports = {
+    createUser,
+    retriveUser,
+}
