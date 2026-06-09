bcf2b3440c5d3ae8c7140927dcfcb2d5c1e77b7e
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-09 21:32:32 +0530

    set user schema for the user model

---
+++ db/userSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    email : {
+        type : String,
+        required: true,
+        trim: true,
+    },
+    password : {
+        type: String,
+        required: true,
+    },
+    role : {
+        type : String,
+        enum : ["user", "organiser", "admin"],
+        default : "user"
+    },
+    bookings : [mongoose.Schema.Types.ObjectId],
+    active : {
+        type: Boolean,
+        default : true
+    },
+    
+},
+{
+        timestamps : true
+    }
+)
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
