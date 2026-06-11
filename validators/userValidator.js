bcaa3133cf30d4d9c521d23acab7be8aa114ac1a
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-11 17:01:04 +0530

    set login validator

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,12 +1,18 @@
 const zod = require("zod");
 
 const authValidation = zod.object({
-    name : zod.string().min(8, "Invalid user name"),
-    email : zod.email("Invalid email"),
-    password : zod.string().min(8, "Invalid password")
+    name : zod.string().min(8, "Invalid username field"),
+    email : zod.email("Invalid email field"),
+    password : zod.string().min(8, "Invalid password field")
 });
+
+const loginValidation = zod.object({
+    email: zod.email("Invalid email"),
+    password : zod.string().min(8, "Invalid password field")
+})
 
 
 module.exports = {
     authValidation,
+    loginValidation,
 }
