5438f702a996f0b2a882e0372e7480f0027448fa
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-17 12:37:26 +0530

    set event validation to check if the request body data is valid or not

---
+++ validators/eventValidator.js
@@ -0,0 +1,17 @@
+const zod = require('zod');
+
+
+const eventValidation = zod.object({
+    name: zod.string().min(5, "Invalid event name"),
+    date : zod.iso.date("Invalid date"),
+    duration : zod.number("Invalid number").positive(),
+    description : zod.string().min(25, "Invalid description"),
+    photo : zod.string().min(5, "Invalid photo directory").optional(),
+    available_tickets : zod.number().positive(),
+    cost : zod.number().positive()
+})
+
+
+module.exports = {
+    eventValidation,
+}
