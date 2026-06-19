dee0e88a8e1cb3faa07775401c759e2801b47876
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-20 04:11:23 +0530

    set get event route with authenticate and connect with the get all the event controller

--- routes/eventOrganiserRoutes.js
+++ routes/eventOrganiserRoutes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 
 const {
     createEventController,
+    retriveAllEventController,
 } = require("../controllers/eventOrganiserControllers");
 
 const {
@@ -19,6 +20,7 @@ const {
 } = require("../middlewares/authMiddleware")
 
 router.post("/event", authenticateUser, authorizeOrganiser, bodyValidatorMiddleware(eventValidation) ,createEventController);
+router.get("/event", authenticateUser, retriveAllEventController);
 
 
 module.exports = router;
