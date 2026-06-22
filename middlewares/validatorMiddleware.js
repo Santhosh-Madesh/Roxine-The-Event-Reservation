d070e8c388a30c83ae8aa45a372c0f20f2d702d7
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-22 16:13:10 +0530

    set query validator middleware

--- middlewares/validatorMiddleware.js
+++ middlewares/validatorMiddleware.js
@@ -40,7 +40,34 @@ const paramValidatorMiddleware = (schema, paramName) => (req, res, next)=>{
     next(createError(400, "Bad request, Missing fields or invalid request field values"));
 }
 
+const queryValidatorMiddleware = (schema) => (req, res, next)=>{
+
+    const validateQuery = schema.safeParse(req.query);
+
+    if(validateQuery.success){
+
+        req.validatedData = {};
+
+        if(req.query.name){
+            req.validatedData.name = req.query.name;
+        }
+
+        if(req.query.date){
+            req.validatedData.date = req.query.date;
+        }
+
+        if(req.query.cost){
+            req.validatedData.cost = Number(req.query.cost);
+        }
+
+        return next();
+    }
+
+    next(createError(400, "Bad request, Missing fields or invalid request field values"));
+}
+
 module.exports = {
     bodyValidatorMiddleware,
     paramValidatorMiddleware,
+    queryValidatorMiddleware,
 }
