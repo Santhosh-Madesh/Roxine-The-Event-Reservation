84146ba148d47269eec41b86e65fe432e12bb12e
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-06-22 16:12:25 +0530

    import query middleware and filter query validation

--- routes/eventOrganiserRoutes.js
+++ routes/eventOrganiserRoutes.js
@@ -5,16 +5,19 @@ const {
     createEventController,
     retriveAllEventController,
     retriveEventByNameController,
+    retriveEventFilterController,
 } = require("../controllers/eventOrganiserControllers");
 
 const {
     bodyValidatorMiddleware,
     paramValidatorMiddleware,
+    queryValidatorMiddleware,
 } = require("../middlewares/validatorMiddleware")
 
 const {
     eventValidation,
     retriveEventValidation,
+    filterQueryValidation,
 } = require("../validators/eventValidator");
 
 const {
@@ -24,7 +27,9 @@ const {
 
 router.post("/event", authenticateUser, authorizeOrganiser, bodyValidatorMiddleware(eventValidation) ,createEventController);
 router.get("/event", authenticateUser, retriveAllEventController);
-router.get("/event/:name", authenticateUser, paramValidatorMiddleware(retriveEventValidation, "name") ,retriveEventByNameController)
+router.get("/event/:name", authenticateUser, paramValidatorMiddleware(retriveEventValidation, "name") ,retriveEventByNameController);
+router.get("/event/filter/query", authenticateUser, queryValidatorMiddleware(filterQueryValidation), retriveEventFilterController);
+
 
 
 module.exports = router;
