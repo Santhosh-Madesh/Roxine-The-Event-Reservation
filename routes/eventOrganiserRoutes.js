03f555e152737f7a9685efd443d35cc382d7f939
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-01 13:31:22 +0530

    seperate user event specific routes

--- routes/eventOrganiserRoutes.js
+++ routes/eventOrganiserRoutes.js
@@ -36,13 +36,8 @@ const {
 } = require("../middlewares/authMiddleware")
 
 router.post("/event", authenticateUser, authorizeOrganiser, bodyValidatorMiddleware(eventValidation) ,createEventController);
-router.get("/event", authenticateUser, retriveAllEventController);
-router.get("/event/:name", authenticateUser, paramValidatorMiddleware(retriveEventValidation) ,retriveEventByNameController);
-router.get("/event/filter/query", authenticateUser, queryValidatorMiddleware(filterQueryValidation), retriveEventFilterController);
-router.get("/paginate/event", authenticateUser, queryValidatorMiddleware(paginationQueryValidation)  ,retrivePaginatedEventController);
 router.delete("/event/:eventId", authenticateUser, authorizeOrganiser, paramValidatorMiddleware(eventIdValidation), authorizeOwner ,deleteEventByIdController);
 router.put("/event/:eventId", authenticateUser, authorizeOrganiser, paramValidatorMiddleware(eventIdValidation), authorizeOwner, bodyValidatorMiddleware(eventValidation), updateEventByIdController);
-router.post("/event/bill", authenticateUser, bodyValidatorMiddleware(billReqValidation) ,generateBillController);
-router.post("/event/payment", authenticateUser, bodyValidatorMiddleware(bookValidation) ,bookTicketsController);
+
 
 module.exports = router;
