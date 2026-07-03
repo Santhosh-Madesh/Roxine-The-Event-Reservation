54b9d411f10c0f858ae3794cc6b47c0ce512fff3
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-03 18:13:51 +0530

    set organisation update request validation

--- validators/organisationValidator.js
+++ validators/organisationValidator.js
@@ -1,10 +1,22 @@
 const zod = require("zod");
+const mongoose = require("mongoose");
 
 
 const organisationReqValidation = zod.object({
     name: zod.string().min(8, "Invalid name field"),
 });
 
+const orgReqUpdateValidation = zod.object({
+    userId: zod.string().refine(
+        (val) => mongoose.Types.ObjectId.isValid(val),
+        "Invalid user id field"
+    ),
+    newObj: zod.object({
+        status: zod.enum(["rejected", "pending", "accepted"])
+    })
+})
+
 module.exports = {
-    organisationReqValidation
+    organisationReqValidation,
+    orgReqUpdateValidation,
 }
