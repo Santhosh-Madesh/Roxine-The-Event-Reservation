687dc0adf7062fef67e5380eff11cf806d566147
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-03 18:13:24 +0530

    set route for updating organisation object

--- routes/organisationRoutes.js
+++ routes/organisationRoutes.js
@@ -3,11 +3,12 @@ const router = require("express").Router();
 const {
     requestOrganisationController,
     organisationStatusController,
-    
+    updateOrganisationRequest,
 } = require("../controllers/eventOrganiserControllers");
 
 const {
     authenticateUser,
+    authorizeAdmin,
 } = require("../middlewares/authMiddleware");
 
 const {
@@ -15,11 +16,13 @@ const {
 } = require("../middlewares/validatorMiddleware")
 
 const {
-    organisationReqValidation
+    organisationReqValidation,
+    orgReqUpdateValidation,
 } = require("../validators/organisationValidator");
 
 
 router.post("/request", authenticateUser, bodyValidatorMiddleware(organisationReqValidation), requestOrganisationController);
 router.get("/request/status", authenticateUser, organisationStatusController);
+router.put("/request/update", authenticateUser, authorizeAdmin, bodyValidatorMiddleware(orgReqUpdateValidation), updateOrganisationRequest);
 
 module.exports = router;
