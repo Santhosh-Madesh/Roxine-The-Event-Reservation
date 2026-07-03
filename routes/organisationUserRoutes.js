ca53dacbc9a8cb8dce132d4117c53c7a61132c33
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-03 23:41:01 +0530

    set crud routes for organisation model to crud organiser

---
+++ routes/organisationUserRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+const {
+    createOrganiserController,
+    updateOrganiserController,
+    deleteOrganiserController
+} = require("../controllers/organisationControllers");
+
+const {
+    authenticateUser,
+    authorizeOrganisation,
+} = require("../middlewares/authMiddleware")
+
+const {
+    bodyValidatorMiddleware,
+} = require("../middlewares/validatorMiddleware");
+
+const {
+    authValidation,
+} = require("../validators/userValidator");
+
+
+
+router.post("/organiser", authenticateUser, authorizeOrganisation, bodyValidatorMiddleware(authValidation) ,createOrganiserController);
+router.put("/organiser", authenticateUser, authorizeOrganisation, updateOrganiserController);
+router.delete("/organiser", authenticateUser, authorizeOrganisation, deleteOrganiserController);
+
+
+
+
+
+
+
