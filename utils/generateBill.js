cc0fd3045595dbde7796b19a5c5612296b149be5
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-06 19:03:48 +0530

    refactor to pass all the current tests

--- utils/generateBill.js
+++ utils/generateBill.js
@@ -2,6 +2,10 @@
 
 const generateBill = (no_of_tickets, cost_per_ticket, tax, platform_fee, payment_status="not paid") =>{
 
+    if(typeof(no_of_tickets) != "number" || typeof(cost_per_ticket) != "number" || typeof(tax) != "number" || typeof(platform_fee) != "number"){
+        return undefined;
+    }
+
     const total_fee = (no_of_tickets*cost_per_ticket) + tax + platform_fee;
 
     const billData = {
