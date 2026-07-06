71ea311c7380d5877844ddf2c4968609a01c7aa4
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-06 22:18:59 +0530

    add more test cases

--- utils.test.js
+++ utils.test.js
@@ -1,4 +1,3 @@
-const { object } = require("zod");
 const generateBill = require("./utils/generateBill");
 
 
@@ -11,7 +10,15 @@ test("should return undefined when cost is not a number", ()=>{
     expect(generateBill(100, "hello", 200, 239)).toBe(undefined);
 })
 
-test("should return an object", ()=>{
+test("should return undefined when tax is not a number", ()=>{
+    expect(generateBill(100, 10, "hello", 239)).toBe(undefined);
+})
+
+test("should return undefined when platform fee is not a number", ()=>{
+    expect(generateBill(100, 10, 200, "hello")).toBe(undefined);
+})
+
+test("should return an appropriate object", ()=>{
     const billObj = {
         no_of_tickets: 10,
         cost_per_ticket : 200,
@@ -26,3 +33,17 @@ test("should return an object", ()=>{
 test("should not return undefined when the inputs are of valid type", ()=>{
     expect(generateBill(10, 100, 20, 30)).not.toBe(undefined);
 })
+
+test("should return undefined when not enough mandatory argument values are provided", ()=>{
+    expect(generateBill(200, 10, 10)).toBe(undefined);
+})
+
+
+test("should return undefined when no argument is given at all", ()=>{
+    expect(generateBill()).toBe(undefined);
+})
+
+test("should return undefined when payment status is neither not paid nor paid", ()=>{
+    expect(generateBill(10, 100, 20, 30, "hola comosta")).toBe(undefined);
+})
+
