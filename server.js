dbda7267f1feacef82d2e557b41d29ff88353dcd
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-07 22:53:36 +0530

    replace orgin url in cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const cors = require("cors");
 
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : "https://roxine-frontend.vercel.app",
     methods : ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders : ['Content-Type', 'Authorization'],
     credentials : true,
